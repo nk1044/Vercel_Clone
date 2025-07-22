7cfd575352790bcb1a4ab2c85c5504643024f256
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-07-22 23:58:05 +0530

    feat: initialize Next.js application with authentication and database setup
    - Added package.json with dependencies for Next.js, authentication, and database.
    - Created PostCSS configuration for Tailwind CSS.
    - Added favicon and various SVG assets for the application.
    - Implemented NextAuth for Google authentication with user login handling.
    - Established MongoDB connection and user model for storing user data.
    - Developed API routes for authentication and a sample hello world endpoint.
    - Created middleware for protected routes.
    - Designed basic authentication page and home page with session handling.
    - Set up global styles using Tailwind CSS and custom CSS variables.
    - Configured TypeScript settings for the application.

---
+++ application/src/lib/models/project.model.ts
@@ -0,0 +1,29 @@
+import {model, Schema} from "mongoose";
+
+const ProjectSchema = new Schema({
+    URL: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name:{
+        type: String,
+    },
+    Owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    status: {
+        type: String,
+        default: "Pending",
+        enum: ["Pending", "Downloading", "Building", "Deploying", "Completed", "Failed"]
+    }
+}, {timestamps: true});
+
+export const Project = model("Project", ProjectSchema);
+
+ProjectSchema.methods.UpdateStatus = async function(status: string) {
+    this.status = status;
+    await this.save({validateBeforeSave: false});
+    return this;
+}
---
+++ application/src/lib/models/user.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { models, Schema, model } from "mongoose";
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://www.gravatar.com/avatar/0000000000000000000000000000000?d=mp&f=y",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    Projects: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Project'
+        }
+    ]
+});
+
+export interface IUser {
+    name: string;
+    email: string;
+    avatar: string;
+    createdAt: Date;
+    Projects: mongoose.Types.ObjectId[];
+}
+
+if(process.env.MODE === "development") {
+    if (models.User) {
+        delete models.User;
+    }
+}
+
+export const User = model("User", userSchema);
